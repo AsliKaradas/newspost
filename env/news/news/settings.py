6523d3701ba793a403f8f934b1c16b20914a2475
Asli Karadas <karadas.asli@gmx.at>
2024-06-06 10:21:15 +0000

    remove dj_database

--- env/news/news/settings.py
+++ env/news/news/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from django.contrib.messages import constants as messages 
 import os
-import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
