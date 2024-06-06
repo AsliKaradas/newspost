16a2f1272d067859ed9340c1fb8fad2ad6bb16f5
Asli Karadas <karadas.asli@gmx.at>
2024-06-06 07:04:56 +0000

    Add

--- env/news/news/settings.py
+++ env/news/news/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^nl@cpb+es54yhf0g0&+ntiw5%@c=xua+4w+@cbp(k6f&_@60+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-aslikaradas-newspost-7fmuidrbjmv.ws-eu114.gitpod.io']
 
 
 # Application definition
