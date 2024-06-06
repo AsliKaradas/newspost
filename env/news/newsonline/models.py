4ea9256690357b6a536fd9ee5b28d36542296571
Asli Karadas <karadas.asli@gmx.at>
2024-06-06 09:59:38 +0000

    remove from models

--- env/news/newsonline/models.py
+++ env/news/newsonline/models.py
@@ -1,15 +1,2 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-
-class BlogModel(models.Model):
-    title = models.CharField(max_length=1000)
-    content = FroalaField()
-    slug = modesl.SlugField(max_length=1000)
-    image = models.ImageField(upload_to='blog')
-    created_at = models.DateTimeField(auto_now_add=True)
-    upload_to = models.DateTimeField(auto_now=True)
-
-
-    def __str__(self):
-        return self.title
