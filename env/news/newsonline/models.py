8941799d7ec2212601e87a1882699b27eed3a23a
Asli Karadas <karadas.asli@gmx.at>
2024-06-06 09:15:31 +0000

    Add content

--- env/news/newsonline/models.py
+++ env/news/newsonline/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from froala_editor.fields import FroalaField
 
-# Create your models here.
+
+class BlogModel(models.Model):
+    title = models.CharFields(max_length=1000)
+    content = FroalaField()
+    slug = modesl.SlugField(max_length=1000)
+    image = models.ImageField(upload_to='blog')
+    created_at = models.DateTimeField(auto_now_add=True)
+    upload_to = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
